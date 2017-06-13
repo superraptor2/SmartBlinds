712d4128462d30d922726486e3c0b22c3fd22e13
Nico <nico.clompen@gmail.com>
2017-06-13 17:22:33 +0200

    Login en macros werken

--- DbClass.py
+++ DbClass.py
@@ -1,3 +1,4 @@
+
 class DbClass:
     def __init__(self):
         import mysql.connector as connector
@@ -11,6 +12,34 @@ class DbClass:
 
         self.__connection = connector.connect(**self.__dsn)
         self.__cursor = self.__connection.cursor()
+
+    def getLogs(self):
+        sqlQuery = "SELECT reden, toestand, datumUur FROM dbsmartblinds.tbllog"
+        self.__cursor.execute(sqlQuery)
+        result = self.__cursor.fetchall()
+        return result
+
+    def getUser(self, username):
+        sqlquery = "SELECT username, password FROM dbsmartblinds.tblusers WHERE username = '{param1}'"
+        sqlCommand = sqlquery.format(param1=username)
+        self.__cursor.execute(sqlCommand)
+        result = self.__cursor.fetchone()
+        self.__cursor.close()
+        return result
+
+    def getDevices(self):
+        query = "SELECT idDevice, devicename FROM dbsmartblinds.tbldevices"
+        self.__cursor.execute(query)
+        result = self.__cursor.fetchall()
+        self.__cursor.close()
+        return result
+
+    def getDevice(self, deviceid):
+        query = "SELECT idDevice, devicename FROM dbsmartblinds.tbldevices WHERE idDevice = '" + deviceid + "'"
+        self.__cursor.execute(query)
+        result = self.__cursor.fetchall()
+        self.__cursor.close()
+        return result
 
     def getDataFromDatabase(self):
         # Query zonder parameters
--- SmartBlinds.py
+++ SmartBlinds.py
@@ -1,7 +1,8 @@
 from flask import Flask
 import os
-from flask import render_template, redirect, url_for, request, session, flash
+from flask import render_template, redirect, url_for, request, session, flash, g
 from functools import wraps
+from DbClass import DbClass
 
 #create the application object
 app = Flask(__name__)
@@ -23,14 +24,30 @@ def login_required(f):
 # use decorators to link the function to a url
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # error = None
+    # if request.method == 'POST':
+    #     if request.form['username'] !='admin' or request.form['password'] != 'admin':
+    #         error = ' invalid credentials, Please try again.'
+    #     else:
+    #         session['logged_in'] = True
+    #         flash('You were just logged in!')
+    #         return redirect(url_for('index'))
+    # return render_template('login.html', error=error)
+
     error = None
-    if request.method == 'POST':
-        if request.form['username'] !='admin' or request.form['password'] != 'admin':
-            error = ' invalid credentials, Please try again.'
+    if request.method == "POST":
+        db = DbClass()
+        user_credentials = db.getUser(request.form['username'])
+        if user_credentials: #Als de lijst NIET leeg is dan...
+            if (user_credentials[1]) != request.form['password']:
+                error = ' Wrong password. Please try again.'
+            else:
+                session['logged_in'] = True
+                flash('You were just logged in!')
+                session['username'] = user_credentials[0]
+                return redirect(url_for("index"))
         else:
-            session['logged_in'] = True
-            flash('You were just logged in!')
-            return redirect(url_for('index'))
+            error = ' User does not excist. Please try again.'
     return render_template('login.html', error=error)
 
 @app.route('/logout')
@@ -42,17 +59,32 @@ def logout():
 @app.route('/')
 @login_required
 def index():
-    return render_template('index.html')
+    #g.db = DbClass()
+    #cur = g.db.execute('select * from posts')
+    #posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
+    #g.db.close()
+    return render_template('index.html') #, posts=posts)
 
-@app.route('/scenes')
+@app.route('/devices')
 @login_required
-def scenes():
-    return render_template('scenes.html')
+def devices():
+    db = DbClass()
+    devicelijst = db.getDevices()
+    return render_template('devices.html', devicelijst=devicelijst)
+
+@app.route('/devicedetail/<deviceid>')
+@login_required
+def devicedetail(deviceid):
+    db = DbClass()
+    devicedetail = db.getDevice(deviceid)
+    return render_template('devicedetail.html' , devicedetail=devicedetail)
 
 @app.route('/logs')
 @login_required
 def logs():
-    return render_template('logs.html')
+    db_layer = DbClass()
+    list_logs = db_layer.getLogs()
+    return render_template('logs.html', list_logs=list_logs)
 
 @app.route('/about')
 @login_required
